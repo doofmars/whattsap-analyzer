bb31a688315cf58bba9764afc43cd56243e9d9d7
doofmars <jan-preuss@gmx.de>
2015-02-08 17:31:49 +0100

    Added timer to determine entire runtime

--- src/main/java/de/doofmars/whattsap/parser/Main.java
+++ src/main/java/de/doofmars/whattsap/parser/Main.java
@@ -8,7 +8,9 @@ package de.doofmars.whattsap.parser;
 public class Main {
 	
 	public static void main(String[] args) {
+		long timer = System.currentTimeMillis();
 		WhatsappMessageImporter importer = new WhatsappMessageImporter();
 		importer.importFromTxt();
+		System.out.println("Elapsed time in ms: " + (System.currentTimeMillis() - timer));
 	}
 }
